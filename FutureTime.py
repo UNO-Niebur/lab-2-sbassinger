be67579ce4d50c8b1a3caca7a89eb01d6f749d8f
sbassinger <35499272@nebraska.edu>
2026-02-02 04:30:09 +0000

    All done here. The minute wrapover was tricky!

--- FutureTime.py
+++ FutureTime.py
@@ -1,7 +1,8 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
+#Name: Scott Bassinger
+#Date: 02/01/2026
+#Assignment: Lab2- FutureTime
+#purpose: Calculate the future time based on user input of hours and minutes to add to the current time.
 
 # datetime will allow us to access the system date and time.
 import datetime
@@ -16,13 +17,20 @@ def main():
 
   #TODO:
   #Ask user for hours
+  usrHours = int(input("Could you provide me with some arbitrary number of hours?"))
   #Ask user for minutes
-
+  usrMinutes = int(input("Okay, and a number of minutes?"))
   #Calculate the time after the user-supplied time has passed.
-
+  totalMinutes = currentMinute + usrMinutes
+  totalHours = currentHour + usrHours + (totalMinutes // 60)
+  futureMinute = totalMinutes % 60
+  futureHour = totalHours % 24
+  futureTime = (futureHour, futureMinute)
+  
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
+  print("The time after the hours and minutes you provided will be: {:02}:{:02}".format(futureTime[0], futureTime[1]))
 
 
 if __name__ == '__main__':
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,18 +1,23 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Scott Bassinger
+#Date: 02/01/2026
+#Assignment: Lab2- Magic8Ball
+#purpose: Create a Magic 8 Ball program that provides random answers to user questions.
 
 #We will need random for this program, import to use this package.
 import random
 
 def main():
   #Create a list of your responses.
+  responses = ["Yes", "No", "Wouldn't you like to know?", "What do you think?", "What would your therapist say?", 
+               "Definitely maybe", "Ask again later", "Indubitably", "The outlook is grim", 
+               "I'm reporting your question to the authorities"]
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  question = input("Ask me anything and I shall answer: ")
   #Answer question randomly with one of the options from your earlier list.
-
+  answer = random.choice(responses)
+  print("The Magic 8 Ball says: " + answer)
 
 if __name__ == '__main__':
   main()
